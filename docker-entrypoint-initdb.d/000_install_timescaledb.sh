46b6a099dec4792f9a23b737ddd2a4a45ff7af49
Robin Thrift <services@robinthrift.com>
2021-05-21 13:38:14 +0200

    Fix bitnami support

--- docker-entrypoint-initdb.d/000_install_timescaledb.sh
+++ docker-entrypoint-initdb.d/000_install_timescaledb.sh
@@ -25,7 +25,7 @@ cat <<EOF >${create_sql}
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 EOF
 
-TS_TELEMETRY='basic'
+TS_TELEMETRY='off'
 if [ "${TIMESCALEDB_TELEMETRY:-}" == "off" ]; then
 	TS_TELEMETRY='off'
 
@@ -38,11 +38,14 @@ fi
 
 echo "timescaledb.telemetry_level=${TS_TELEMETRY}" >> ${POSTGRESQL_CONF_DIR}/postgresql.conf
 
+export PGPASSWORD="${POSTGRES_POSTGRES_PASSWORD}"
 
 # create extension timescaledb in initial databases
-psql -U "${POSTGRES_USER}" postgres -f ${create_sql}
-psql -U "${POSTGRES_USER}" template1 -f ${create_sql}
+psql -U "postgres" --dbname=postgres -f ${create_sql}
+psql -U "postgres" --dbname=template1 -f ${create_sql}
+
+export PGPASSWORD="${POSTGRES_PASSWORD}"
 
 if [ "${POSTGRES_DB:-postgres}" != 'postgres' ]; then
-    psql -U "${POSTGRES_USER}" "${POSTGRES_DB}" -f ${create_sql}
+    psql -U "${POSTGRES_USER}" --dbname="${POSTGRES_DB}" -f ${create_sql}
 fi
